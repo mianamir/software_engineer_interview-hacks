43e5afc6bd25945e43c944061cc8d24b365282df
mianamir <mianamirlahore@gmail.com>
2020-09-02 21:20:52 +0500

    advance Python, Django, SQL concepts

---
+++ advance_python_django_sql.py
@@ -0,0 +1,38 @@
+"""
+Python memory management
+
+Python garabag collector checks object references in the code/program
+if any object reference is not using in the code, then Python
+garbage collector delete that object
+
+https://docs.python.org/3/c-api/memory.html#:~:text=Overview,by%20the%20Python%20memory%20manager.
+https://realpython.com/python-memory-management/
+
+
+Python decorator function
+ in the decorator, function remember its last state using yield
+
+
+Django ORM
+raw querries
+Django models, reverse relation, on delete CASCADE
+
+REST APIS State management
+CORS
+
+Cross site scripting
+
+Hashing / Encreption for Register/Login Django
+
+SQL cardnality concept
+
+Scrapy / Python scraping
+
+REACT apis routing
+
+
+
+
+
+
+"""
