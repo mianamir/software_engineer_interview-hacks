6114775bb28df679faaa7e71c17bdbdbf02fbccb
mianamir <mianamirlahore@gmail.com>
2020-09-03 17:27:56 +0500

    Tricky programs

---
+++ tricky_programs.py
@@ -0,0 +1,16 @@
+"""
+
+Prime numbers
+"""
+
+number = int(input("Enter number: "))
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(f"{number} is not prime number")
+            print(f"{i}, times, {number//i} is {number}")
+            break
+    else:
+        print(f"{number} is prime number")
+else:
+    print(f"{number} is not prime number")
